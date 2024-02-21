0b967adc60d42506282bd3f94e109ecbf7f59b8b
yasin-hsi <yasin.junet@gmail.com>
2024-02-21 22:05:37 +0700

    feat: home page banner carousel

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,3 +1,3 @@
 const String appVersion = 'v.2401-2001';
-const String userName = 'Yasin Junet';
+const String userName = 'YASIN JUNET';
 const String userId = 'ARN232-09102';
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,8 +1,24 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:hsi_app/config/config.dart' as config;
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
+
+  static List<Image> banners = [
+    Image.asset(
+      'assets/images/banner/banner-1.jpg',
+      fit: BoxFit.cover,
+    ),
+    Image.asset(
+      'assets/images/banner/banner-2.jpg',
+      fit: BoxFit.cover,
+    ),
+    Image.asset(
+      'assets/images/banner/banner-3.jpg',
+      fit: BoxFit.cover,
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,14 +45,54 @@ class HomeView extends StatelessWidget {
         body: ListView(
           children: [
             Container(
-                margin: const EdgeInsets.all(10),
-                child: const Stack(children: [
+                padding: const EdgeInsets.all(20),
+                child: Stack(children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('Assalamualaikum,'),
-                      Text(config.userName),
-                      Text(config.userId)
+                      const Text('Assalamualaikum,',
+                          style: TextStyle(color: Color(0xFF898989))),
+                      const SizedBox(height: 5),
+                      const Text(
+                        config.userName,
+                        style: TextStyle(
+                            fontSize: 28, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        config.userId,
+                        style: TextStyle(
+                            color: Color(0xFF666666),
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 15),
+                      CarouselSlider(
+                        options: CarouselOptions(
+                          height: 240.0,
+                          autoPlay: true,
+                        ),
+                        items: banners.map((bannerImage) {
+                          return Builder(
+                            builder: (BuildContext context) {
+                              return Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 10),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    border: Border.all(
+                                        color: Colors.grey.withOpacity(0.2)),
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(5),
+                                    child: bannerImage,
+                                  ));
+                            },
+                          );
+                        }).toList(),
+                      )
                     ],
                   )
                 ]))
