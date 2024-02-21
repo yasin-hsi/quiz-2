af3f498359a622b9db9d5b9841a379019e5fc583
yasin-hsi <yasin.junet@gmail.com>
2024-02-21 21:18:26 +0700

    feat: add config file

---
+++ lib/config/config.dart
@@ -0,0 +1,3 @@
+const String appVersion = 'v.2401-2001';
+const String userName = 'Yasin Junet';
+const String userId = 'ARN232-09102';
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hsi_app/config/config.dart' as config;
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -6,32 +7,40 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        backgroundColor: const Color(0xFF233975),
-        leading: Container(
-            height: 40,
-            margin: const EdgeInsets.only(left: 10),
-            child: Image.asset('assets/images/logo.png')),
-        title: const Text(
-          'EDU HSI',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        actions: const [
-          Text(
-            'v.2402-1601',
-            style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          centerTitle: false,
+          backgroundColor: const Color(0xFF233975),
+          leading: Container(
+              height: 40,
+              margin: const EdgeInsets.only(left: 10),
+              child: Image.asset('assets/images/logo.png')),
+          title: const Text(
+            'EDU HSI',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
-          SizedBox(width: 10)
-        ],
-      ),
-      body: const Center(child: Text('HOME')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).pushNamed('/profile');
-        },
-        child: const Icon(Icons.navigation),
-      ),
-    );
+          actions: const [
+            Text(
+              config.appVersion,
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(width: 10)
+          ],
+        ),
+        body: ListView(
+          children: [
+            Container(
+                margin: const EdgeInsets.all(10),
+                child: const Stack(children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Assalamualaikum,'),
+                      Text(config.userName),
+                      Text(config.userId)
+                    ],
+                  )
+                ]))
+          ],
+        ));
   }
 }
